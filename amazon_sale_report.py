e8c54171c3cb17e598e176ff17a26adab1fb12b4
abdul Razzaq <ar8937562@gmail.com>
2024-02-03 18:28:02 +0500

    cleaned the data. converted some data types and renamed columns

--- amazon_sale_report.py
+++ amazon_sale_report.py
@@ -10,8 +10,20 @@ data = pd.read_csv("Amazon Sale Report.csv",encoding="unicode_escape")
 # print(data.info())
 
 
-# print(data.drop(['New','PendingS'],axis=1,inplace=True))         # drop unrelated or blank colums.
-# print(df.info())
+print(data.drop(['New','PendingS'],axis=1,inplace=True))     # drop unrelated or blank colums.
+# print(data.info())
 
-# print(data.isnull().sum())                                       # Checking null values
-print(data.dropna(inplace=True))
+print(data.isnull().sum())                                       # Checking null values
+print(data.dropna(inplace=True))                                   # Dropped null values 
+
+data['ship-postal-code'] = data['ship-postal-code'].astype('int')      # converting data type of ship_postal_code
+print(data['ship-postal-code'].dtype)
+
+
+data['Date'] = pd.to_datetime(data['Date'])                            # converting data type of Date
+print(data['Date'].dtype)
+
+
+df = data.rename(columns={'Qty':'Quantity'})                         # renaming the coloumn of Qty
+print(df.columns)
+print(df)
