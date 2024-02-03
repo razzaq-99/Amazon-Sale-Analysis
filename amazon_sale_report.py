a78850a5e8fddad36ef0a5ed23d0214f7f22a199
abdul Razzaq <ar8937562@gmail.com>
2024-02-03 23:17:33 +0500

    Analyzing data for the size and plotting actual size of products

--- amazon_sale_report.py
+++ amazon_sale_report.py
@@ -13,17 +13,39 @@ data = pd.read_csv("Amazon Sale Report.csv",encoding="unicode_escape")
 print(data.drop(['New','PendingS'],axis=1,inplace=True))     # drop unrelated or blank colums.
 # print(data.info())
 
-print(data.isnull().sum())                                       # Checking null values
+# print(data.isnull().sum())                                       # Checking null values
 print(data.dropna(inplace=True))                                   # Dropped null values 
 
-data['ship-postal-code'] = data['ship-postal-code'].astype('int')      # converting data type of ship_postal_code
-print(data['ship-postal-code'].dtype)
+# data['ship-postal-code'] = data['ship-postal-code'].astype('int')      # converting data type of ship_postal_code
+# print(data['ship-postal-code'].dtype)
 
 
-data['Date'] = pd.to_datetime(data['Date'])                            # converting data type of Date
-print(data['Date'].dtype)
+# data['Date'] = pd.to_datetime(data['Date'])                            # converting data type of Date
+# print(data['Date'].dtype)
 
 
-df = data.rename(columns={'Qty':'Quantity'})                         # renaming the coloumn of Qty
-print(df.columns)
-print(df)
+# df = data.rename(columns={'Qty':'Quantity'})                         # renaming the coloumn of Qty
+# print(df.columns)
+# print(df)
+
+
+
+
+
+df = sns.countplot(data=data,x="Size" )
+# plt.show()
+
+for bars in df.containers:                                     # shows actual size
+    df.bar_label(bars)
+    plt.savefig("size.png")
+    plt.show()
+    
+    
+    # Note : From above Graph you can see that most people buys Medium (M) size.
+    
+    
+    
+
+
+
+    
