a458e39c7309b3e87d0efbd9baddcad4efecc626
abdul Razzaq <ar8937562@gmail.com>
2024-02-04 16:03:25 +0500

    analyzing data of courier status and the products which sold the most

--- amazon_sale_report.py
+++ amazon_sale_report.py
@@ -7,6 +7,7 @@ import seaborn as sns
 data = pd.read_csv("Amazon Sale Report.csv",encoding="unicode_escape")
 
 # print(data)
+
 # print(data.info())
 
 
@@ -32,20 +33,54 @@ print(data.dropna(inplace=True))                                   # Dropped nul
 
 
 
-df = sns.countplot(data=data,x="Size" )
+# df = sns.countplot(data=data,x="Size" )
 # plt.show()
 
-for bars in df.containers:                                     # shows actual size
-    df.bar_label(bars)
-    plt.savefig("size.png")
-    plt.show()
+# for bars in df.containers:                                     # shows actual size
+    # df.bar_label(bars)
+    # plt.savefig("size.png")
+    # plt.show()
     
     
     # Note : From above Graph you can see that most people buys Medium (M) size.
     
     
-    
+
+# s_q = data.groupby(['Size'],as_index=False)['Qty'].sum().sort_values(by='Qty',ascending=False)
+
+# sns.barplot(x="Size",y="Qty",data=s_q,color='orange')
+# plt.show()
 
 
 
-    
+
+
+ # Courier status :
+# sns.countplot(data = data, x="Courier Status",hue="Status",palette='viridis')
+# plt.show()
+
+
+# Note : From above Graph you can see that mostly items are shipped and few are cancelled due to some sort of reasons.
+       
+       
+ 
+
+
+# Checking which product is sold the most :
+data['Category'] = data['Category'].astype(str)
+
+plt.hist(data = data,x="Category",bins=20,edgecolor='black',color='purple')
+plt.xlabel("Products",fontsize=12)
+plt.ylabel("Quantity",fontsize=12)
+plt.title("Product sold the most",fontsize=15)
+plt.show()
+
+
+#  Note : From above graph you can see that T-shirts and shirts are the two most sold products. But some other products like shoes,
+#         socks , wallet and perfume are not so popular.
+
+
+
+
+
+
